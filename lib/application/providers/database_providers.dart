81a25bdb51ed9685323c80537ec699f523b698b5
Anees040 <sp23-bse-030@isbstudent.comsats.edu.pk>
2026-07-27 02:52:05 +0500

    feat: implement database_providers.dart for A4 milestone

---
+++ lib/application/providers/database_providers.dart
@@ -0,0 +1,49 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:sqflite/sqflite.dart';
+import 'package:fitpilot/data/local/app_database.dart';
+import 'package:fitpilot/data/repositories/burn_repository.dart';
+import 'package:fitpilot/data/repositories/exercise_repository.dart';
+import 'package:fitpilot/data/repositories/food_repository.dart';
+import 'package:fitpilot/data/repositories/log_repository.dart';
+import 'package:fitpilot/data/repositories/profile_repository.dart';
+
+/// Provides the initialized Database instance.
+/// It is safe to use `AppDatabase.instance()` asynchronously since it's a singleton,
+/// but providing it via a FutureProvider makes it easy for other providers to await it.
+final databaseProvider = FutureProvider<Database>((ref) async {
+  return await AppDatabase.instance();
+});
+
+/// Exposes the FoodRepository.
+final foodRepositoryProvider = FutureProvider<FoodRepository>((ref) async {
+  final db = await ref.watch(databaseProvider.future);
+  return FoodRepository(db);
+});
+
+/// Exposes the LogRepository.
+final logRepositoryProvider = FutureProvider<LogRepository>((ref) async {
+  final db = await ref.watch(databaseProvider.future);
+  return LogRepository(db);
+});
+
+/// Exposes the BurnRepository.
+final burnRepositoryProvider = FutureProvider<BurnRepository>((ref) async {
+  final db = await ref.watch(databaseProvider.future);
+  return BurnRepository(db);
+});
+
+/// Exposes the ProfileRepository.
+final profileRepositoryProvider = FutureProvider<ProfileRepository>((
+  ref,
+) async {
+  final db = await ref.watch(databaseProvider.future);
+  return ProfileRepository(db);
+});
+
+/// Exposes the ExerciseRepository.
+final exerciseRepositoryProvider = FutureProvider<ExerciseRepository>((
+  ref,
+) async {
+  final db = await ref.watch(databaseProvider.future);
+  return ExerciseRepository(db);
+});
