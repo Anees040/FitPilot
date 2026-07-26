fdaf8f92be515ae913501c82f6265ef0ebd051f5
Anees040 <sp23-bse-030@isbstudent.comsats.edu.pk>
2026-07-27 02:52:10 +0500

    feat: implement progress_screen.dart for A4 milestone

--- lib/features/progress/presentation/progress_screen.dart
+++ lib/features/progress/presentation/progress_screen.dart
@@ -6,11 +6,7 @@ class ProgressScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const Scaffold(
-      body: SafeArea(
-        child: Center(
-          child: Text('Progress'),
-        ),
-      ),
+      body: SafeArea(child: Center(child: Text('Progress'))),
     );
   }
 }
