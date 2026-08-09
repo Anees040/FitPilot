b5b6aac01295b7d4bd7dc75e855be37b22acf36a
Anees040 <sp23-bse-030@isbstudent.comsats.edu.pk>
2026-08-09 18:14:04 +0500

    chore: codebase cleanup and dead asset removal (A0-A8)
    - Fixed .gitignore null bytes
    - Removed dead Dart code (demo_provider, permission_service, quota_policy)
    - Deleted unused UI widgets and ~3.5MB of unused illustration assets
    - Removed permission_handler dependency
    - Cleaned up root junk files
    - Pinned sqflite_common_ffi version

--- lib/application/providers/demo_provider.dart
+++
@@ -1,8 +0,0 @@
-import 'package:flutter_riverpod/flutter_riverpod.dart';
-
-
-final demoProvider = Provider<bool>((ref) {
-  // If we have an env variable for demo mode, we could read it here,
-  // but for now we default to false.
-  return false;
-});
--- test/widget_test.dart
+++
@@ -1,7 +0,0 @@
-import 'package:flutter_test/flutter_test.dart';
-
-void main() {
-  test('A1 nav shell test deprecated by A4 bootstrap', () {
-    expect(true, isTrue);
-  });
-}
